34761288ceaf06ff68158d07e37807054468ecc2
Benjamin Shafer <contrariwiseowl@gmail.com>
2021-04-10 16:11:30 -0600

    Adding views and mock data

---
+++ src/assets/mock-data.js
@@ -0,0 +1,32 @@
+let mock = [
+    {
+        id: 1,
+        title: "Haiku 1",
+        content: ["Five, seven, and five", "Syllables in a haiku", "Let's hope that this works"],
+        author: "Ben Shadow",
+        form: "Haiku"
+    },
+    {
+        id: 2,
+        title: "Haiku & U",
+        content: ["Welcome to haiku", "My teacher told me this form", "Might be Japanese"],
+        author: "Ricky Smith",
+        form: "Haiku"
+    },
+    {
+        id: 3,
+        title: "The Test Drive",
+        content: ["The last train leaving has never left", "Why now, I wonder, it goes?", "Will it come back?", "Who knows?"],
+        author: "Engineer 13",
+        form: "Quatrain"
+    },
+    {
+        id: 4,
+        title: "ERROR",
+        content: ["No data to read", "No data to write", "Workspace is empty", "Queue clear for the night", "XJOBS_DONE => SHUT_DOWN_FATAL"],
+        author: "Peter Compewter",
+        form: "None"
+    }
+]
+
+export default mock;
