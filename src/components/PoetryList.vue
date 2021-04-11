f8436ad900412010fb1643433465ab2ddc44cdd4
Benjamin Shafer <contrariwiseowl@gmail.com>
2021-04-10 20:12:11 -0600

    script added to poetrylist component

---
+++ src/components/PoetryList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div class="poem" v-for="poem in poems" :key="poem.id">
+            <p><strong>{{poem.title}}</strong> by {{poem.author}}</p>
+            <div class="stanza">
+                <ul>
+                    <li v-for="line in poems.lines" :key="line.charAt(0)"></li>
+                </ul>
+            </div>
+        </div>
+    </div>             
+</template>
+
+<script>
+export default {
+    name: 'PoetryList',
+    props: {
+        poems: Array
+    },
+    data: {
+        noName: true
+    }
+}
+</script>
